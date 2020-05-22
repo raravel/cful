5fe6f29d55ae7dd19f5884af9d802e7a7007f118
Tree-Some <youn@tree-some.dev>
2020-05-22 12:04:24 +0900

    true color 지원하지 않으면 ansi color로 사용함. example 추가

--- cful.h
+++ cful.h
@@ -2,13 +2,20 @@
 #define __CFUL_HEADER__
 
 
+#ifndef _INC_STDIO
+#include <stdio.h>
+#endif
+
+#define SET_FG_COLOR(r, g, b)		SET_COLOR(FOREGROUND, r, g, b)
+#define SET_BG_COLOR(r, g, b)		SET_COLOR(BACKGROUND, r, g, b)
+
 #define BOLD		"\033[1m"
 #define DIM			"\033[2m"
 #define UNDERLINE	"\033[4m"
 #define BLINK		"\033[5m"
 #define INVERTED	"\033[7m"
 #define HIDDEN		"\033[8m"
-#define RESET		"\033[0;0;0m"
+#define RESET		"\033[0m"
 
 #define FOREGROUND	"38"
 #define BACKGROUND	"48"
@@ -16,12 +23,47 @@
 #define FG_COLOR(r, g, b)	"\033[" FOREGROUND ";2;"#r";"#g";"#b"m"
 #define BG_COLOR(r, g, b)	"\033[" BACKGROUND ";2;"#r";"#g";"#b"m"
 
-#ifndef _INC_STDIO
-#include <stdio.h>
+int _global_cful_var_use_ansi = 0;
+#define SET_ANSI_MODE(mode) _global_cful_var_use_ansi = mode
+
+#define SET_COLOR(type, r, g, b)	do { \
+	if ( _global_cful_var_use_ansi == 1 ) { \
+		printf("\033[%s;5;%dm", type, rgb_ansi(r, g, b)); \
+	} else { \
+		printf("\033[%s;2;%d;%d;%dm", type, r, g, b); \
+	} \
+} while(0)
+
+inline unsigned char rgb_ansi(unsigned char r, unsigned char g, unsigned char b)
+{
+	// 16: black, 231: white, 6: light blue, 36: blue green
+	int ansi = 16;
+
+	if ( r == g && g == b ) {
+		if ( r < 8 ) {
+			return 16;
+		}
+
+		if ( r > 248 ) {
+			return 231;
+		}
+
+		return (((r - 8) / 247) * 24) + 232;
+	}
+
+	ansi += (int)(36 * ((double)r / 255 * 5));
+	ansi += (int)(6 * ((double)g / 255 * 5));
+	ansi += (int)((double)b / 255 * 5);
+
+	return ansi;
+}
+
+#if !defined(_F) && defined(FG_COLOR)
+#define _F FG_COLOR
 #endif
 
-#define SET_COLOR(type, r, g, b)	printf("\033[%s;2;%d;%d;%dm", type, r, g, b)
-#define SET_FG_COLOR(r, g, b)		SET_COLOR(FOREGROUND, r, g, b)
-#define SET_BG_COLOR(r, g, b)		SET_COLOR(BACKGROUND, r, g, b)
-
+#if !defined(_B) && defined(BG_COLOR)
+#define _B BG_COLOR
 #endif
+
+#endif // __CFUL_HEADER__
---
+++ example/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "../cful.h"
+
+int main ( void )
+{
+	printf(BOLD "Hello World" RESET "\n");
+	printf(_F(255, 196, 0) "Hello World" RESET "\n");
+	printf(_B(135, 219, 70) "Hello World" RESET "\n");
+	printf(_F(0, 0, 0) _B(255,255,255) "Hello World" RESET "\n");
+
+	SET_COLOR(FOREGROUND, 129, 56, 224);
+	printf("Hello World\n");
+
+	SET_COLOR(BACKGROUND, 217, 255, 252);
+	printf("Hello World" RESET "\n");
+
+	SET_ANSI_MODE(1);
+
+	SET_COLOR(FOREGROUND, 255, 85, 0);
+	printf("Hello World\n");
+
+	SET_COLOR(BACKGROUND, 11, 0, 0);
+	printf("Hello World" RESET "\n");
+	return 0;
+}
